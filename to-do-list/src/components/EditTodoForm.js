c0bdd8a4a320cb4bf7d41784443fbedffd0dcff7
Luka Chemia <lchemia@aldagi.ge>
2024-05-28 12:01:07 +0400

    to-do-list done

--- to-do-list/src/components/EditTodoForm.js
+++ to-do-list/src/components/EditTodoForm.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const EditTodoForm = ({editTodo, task}) => {
     const [value, setValue] = useState(task.task);
@@ -12,7 +14,7 @@ export const EditTodoForm = ({editTodo, task}) => {
   return (
     <form onSubmit={handleSubmit} className="TodoForm">
     <input type="text" value={value} onChange={(e) => setValue(e.target.value)} className="todo-input" placeholder='Update task' />
-    <button type="submit" className='todo-btn'>Add Task</button>
+    <button type="submit" className='todo-btn'><FontAwesomeIcon className="edit-icon" icon={faPenToSquare}  /></button>
   </form>
   )
 }
